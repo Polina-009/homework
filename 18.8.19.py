bc5e9d8da08048072e669c34939737361a8ae8e0
Polina <121357389+Polina-009@users.noreply.github.com>
2023-02-07 18:48:37 +0500

    Update 18.8.19.py

--- 18.8.19.py
+++ 18.8.19.py
@@ -2,14 +2,14 @@ ticket = int(input('Сколько билетов вы хотите приобр
 age = []
 for i in range(1, ticket + 1):
     age.append(int(input(f'Введите возраст {i} посетителя: ')))
-    def ticket_prise(age):
-        if age < 18:
-            return 0
-        elif 18 <= age <= 25:
-            return 990
-        elif 25 < age:
-            return 1390
-    total = sum(map(ticket_prise, age))
+def ticket_prise(age):
+    if age < 18:
+         return 0
+    elif 18 <= age <= 25:
+        return 990
+    elif 25 < age:
+        return 1390
+total = sum(map(ticket_prise, age))
 discounted_prise = int(total * 0.9)
 if ticket > 3:
     print('Общая стоимость билетов:', discounted_prise, "руб.")
